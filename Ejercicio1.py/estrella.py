49472092b74f281d0011b15ca41455ee0a91422f
adriangonzalezx <adrigofe@myuax.com>
2024-02-29 19:57:51 +0100

    últimos cambios y confirmación

---
+++ Ejercicio1.py/estrella.py
@@ -0,0 +1,13 @@
+from vector3d import Vector3D
+import random
+
+class Estrella:
+    nombres_galaxias = ["Godzilla", "Vía Entera", "Bocata", "Chocolate", "Badulaque"]
+    
+    def __init__(self, nombre):
+        self.nombre = nombre
+        self.posicion = Vector3D(random.uniform(-1000, 1000), random.uniform(-1000, 1000), random.uniform(-1000, 1000))
+        self.galaxia = random.choice(Estrella.nombres_galaxias)
+    
+    def __str__(self):
+        return f"Estrella {self.nombre} en {self.posicion}, Galaxia {self.galaxia}"
